5d12fe482bd99234e4e9264ea82d42769d6d337a
Nastia Sydorchuk <nastia.sydorchuk@gmail.com>
2022-10-17 00:57:29 +0100

    form

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+# Landing page for Miami Condo Kings
+  Responsive landing page. Hamburger menu, responsive gallery, JavaScript form validation.
+  Technologies: HTML, CSS, Sass (SCSS), BEM, JavaScript.
+
+  - [DEMO LINK](https://nastia-sydorchuk.github.io/miami-condo/)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,82 @@
 'use strict';
 
+// Disable scroll
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
+});
+
+// Form validation
+
+const form = document.querySelector('form');
+const nameInput = document.querySelector('input[name="name"]');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+const successMessage = document.querySelector('.contact-us__message');
+
+const inputs = [emailInput, messageInput, nameInput];
+let formIsValid = false;
+let isValidating = false;
+
+const isEmailValid = (email) => {
+  // eslint-disable-next-line
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return regex.test(email.toLowerCase());
+};
+
+const resetInput = (element) => {
+  element.previousElementSibling.classList.add('form-field__message--hidden');
+  element.classList.remove('form-field--invalid');
+};
+
+const invalidateInput = (element) => {
+  element.previousElementSibling.classList
+    .remove('form-field__message--hidden');
+  element.classList.add('form-field--invalid');
+};
+
+const validateInputs = () => {
+  if (!isValidating) {
+    return;
+  }
+
+  inputs.forEach(resetInput);
+  formIsValid = true;
+
+  if (!nameInput.value.trim()) {
+    invalidateInput(nameInput);
+    formIsValid = false;
+  }
+
+  if (!messageInput.value.trim()) {
+    invalidateInput(messageInput);
+    formIsValid = false;
+  }
+
+  if (!isEmailValid(emailInput.value)) {
+    invalidateInput(emailInput);
+    formIsValid = false;
+  }
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  isValidating = true;
+  validateInputs();
+
+  if (formIsValid) {
+    form.remove();
+    successMessage.classList.remove('contact-us__message--hidden');
+  }
+});
+
+inputs.forEach((input) => {
+  input.addEventListener('input', () => {
+    resetInput(input);
+  });
 });
